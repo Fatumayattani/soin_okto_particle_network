c4f20c5ee0a41bf9ab90c1cf907a4b3b32368c13
Fatumayattani <fyattani@gmail.com>
2024-11-05 12:21:20 +0300

    save changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { OktoProvider, BuildType } from 'okto-sdk-react';
+import App from './App';
 import './index.css';
+
+const GOOGLE_CLIENT_ID = '809424109258-oecvao8rj793m1mhkav5kht508mn9tk4.apps.googleusercontent.com';
+const OKTO_CLIENT_API_KEY = 'your-okto-api-key';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+      <OktoProvider apiKey={OKTO_CLIENT_API_KEY} buildType={BuildType.SANDBOX}>
+        <App />
+      </OktoProvider>
+    </GoogleOAuthProvider>
   </StrictMode>
 );
