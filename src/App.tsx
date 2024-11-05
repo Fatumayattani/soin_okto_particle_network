e8f61dd2a4b77f299aa14f7bf4c79bbb68ca0414
Fatumayattani <fyattani@gmail.com>
2024-11-05 12:13:47 +0300

    save changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import ChatWidget from './components/ChatWidget';
 import HomePage from './pages/HomePage';
+import AuthPage from './pages/AuthPage';
 import CommunityPage from './pages/CommunityPage';
 import SupportGroupsPage from './pages/SupportGroupsPage';
 import ResourcesPage from './pages/ResourcesPage';
@@ -14,6 +16,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/auth" element={<AuthPage />} />
           <Route path="/community" element={<CommunityPage />} />
           <Route path="/support-groups" element={<SupportGroupsPage />} />
           <Route path="/resources" element={<ResourcesPage />} />
